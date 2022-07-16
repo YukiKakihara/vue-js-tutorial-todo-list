44cb30ca8e6f47c4bcbc752ab1c0693faf79569b
yuki.kakihara <argentina.kyg@gmail.com>
2022-07-16 11:12:54 +0900

    ローカルストレージ API の使用

--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
+var STORAGE_KEY = 'todos-vuejs-demo';
+
+var todoStorage = {
+  fetch: function() {
+    var todos = JSON.parse(
+      localStorage.getItem(STORAGE_KEY) || '[]'
+    )
+    todos.forEach(function(todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function(todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+};
+
 const app = new Vue({
   el: '#app',
   data: {
@@ -6,4 +24,4 @@ const app = new Vue({
   methods: {
     // 使用するメソッド
   }
-})
+});
